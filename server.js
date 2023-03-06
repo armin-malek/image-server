9e539e069e1a137179b753a7c3fbf0bdf1d91ac9
dudefromhell <armine.s888@gmail.com>
2023-03-06 15:30:29 +0330

    ataafshar

---
+++ server.js
@@ -0,0 +1,6 @@
+const express = require("express");
+const app = express();
+const port = 4000;
+
+app.get("/", (req, res) => res.send("Hello boys!"));
+app.listen(port, () => console.log(`Example app listening on port ${port}!`));
