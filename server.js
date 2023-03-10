e0f110b2bb173abedbcae58cc708d9e73debd110
Armin Esmaeili <33123807+armin-malek@users.noreply.github.com>
2023-03-10 19:36:58 +0330

    clean up

--- server.js
+++
@@ -1,6 +0,0 @@
-const express = require("express");
-const app = express();
-const port = 6000;
-
-app.get("/", (req, res) => res.send("Hello World!"));
-app.listen(port, () => console.log(`Example app listening on port ${port}!`));
