aacf1bd06a68d8f0eb85228b20a768f5c6217d40
Ali-faridhashemi <126474760+Ali-faridhashemi@users.noreply.github.com>
2023-05-14 22:58:48 +0330

    روتر مسیر ثبت نام

--- backend/routes/auth/index.js
+++ backend/routes/auth/index.js
@@ -2,4 +2,5 @@ const express = require("express");
 const router = express.Router();
 
 router.use("/login", require("./login.js"));
+router.use("/signup", require("./signup.js"));
 module.exports = router;
