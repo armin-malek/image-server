a87ed385b33d16e845b8d6e53fa9e0c707fff9c9
Meusamya <126474382+Meusamya@users.noreply.github.com>
2023-05-28 22:38:45 +0330

    افزودن مسیز درخواست ایمیل

--- backend/routes/auth/index.js
+++ backend/routes/auth/index.js
@@ -3,4 +3,7 @@ const router = express.Router();
 
 router.use("/login", require("./login.js"));
 router.use("/signup", require("./signup.js"));
+
+router.use(require("../../middleware/jwt.js"));
+router.use("/email-req", require("./email-req.js"));
 module.exports = router;
