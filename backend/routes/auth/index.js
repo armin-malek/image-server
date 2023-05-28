83ffe9a11e5a14b55d5a33aefdac05c0e2816846
ethanfazeli <74993523+ethanfazeli@users.noreply.github.com>
2023-05-28 23:41:21 +0330

    افزودن مسیر لندیگ تایید ایمیل

--- backend/routes/auth/index.js
+++ backend/routes/auth/index.js
@@ -4,6 +4,7 @@ const router = express.Router();
 router.use("/login", require("./login.js"));
 router.use("/signup", require("./signup.js"));
 
+router.use("/email-verify", require("./email-verify.js"));
 router.use(require("../../middleware/jwt.js"));
 router.use("/email-req", require("./email-req.js"));
 module.exports = router;
