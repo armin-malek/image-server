d6e1458dff2e81e9922b6e9721b413e0ff194204
dudefromhell <armine.s888@gmail.com>
2023-05-29 07:49:54 +0330

    cors and dashboard data

--- backend/routes/cp/index.js
+++ backend/routes/cp/index.js
@@ -1,9 +1,35 @@
 const express = require("express");
+const { prisma } = require("../../lib/db");
 const router = express.Router();
 router.use(require("../../middleware/jwt"));
 
-router.get("/", (req, res) => {
-  res.send("cp");
+router.get("/", async (req, res) => {
+  try {
+    const user = await prisma.user.findUnique({
+      where: { id: req.user.id },
+      select: {
+        email: true,
+        emailVerified: true,
+        fullName: true,
+        Customer: {
+          select: {
+            storageUsed: true,
+            trafficUsed: true,
+            _count: { select: { Files: true } },
+          },
+        },
+      },
+    });
+
+    // tabdil BigInt ha be string
+    user.Customer.storageUsed = user.Customer.storageUsed.toString();
+    user.Customer.trafficUsed = user.Customer.trafficUsed.toString();
+
+    res.send({ ok: true, user });
+  } catch (err) {
+    console.log(err);
+    res.status(500).send("خطایی رخ داد");
+  }
 });
 router.use("/library", require("./library"));
 module.exports = router;
--- backend/server.js
+++ backend/server.js
@@ -2,9 +2,15 @@ require("dotenv").config();
 const express = require("express");
 const app = express();
 const port = process.env.PORT || 4000;
-
+const cors = require("cors");
 const requestIp = require("request-ip");
 
+var corsOptions = {
+  origin: ["http://localhost:8080"],
+  optionsSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204
+};
+
+app.use(cors(corsOptions));
 app.use(requestIp.mw());
 app.use(express.json({ limit: "20mb" }));
 app.use(express.static("./public"));
